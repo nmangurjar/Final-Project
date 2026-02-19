5bcf63eb0f47b3ddd5a290c74a51fa90b78acda9
Mohith Sai Atluri <MohithSai.Atluri@cognizant.com>
2026-02-19 18:17:06 +0530

    Need to filter events based on price

---
+++ src/test/java/components/Events.java
@@ -0,0 +1,43 @@
+package components;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+
+public class Events {
+    private static void selectThisWeekendButton(WebDriver driver) {
+        WebElement wkndBtn = Constants.waitUntil(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(EventComponentLocators.thisWeekendBtn)));
+        Actions actions = new Actions(driver);
+
+        actions.moveToElement(wkndBtn).click().perform();
+    }
+
+    private static void applyPriceFilter(WebDriver driver) {
+        driver.findElement(By.xpath(EventComponentLocators.filterBtn)).click();
+        driver.findElement(By.xpath(EventComponentLocators.filterLabel)).click();
+        driver.findElement(By.xpath(EventComponentLocators.applyFilters)).click();
+    }
+
+    private static void saveEvents(WebDriver driver) {
+        List<WebElement> events = Constants.waitUntil(driver, 10).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(EventComponentLocators.eventAnchor)));
+        for(WebElement event: events) {
+            System.out.println(event.getText());
+        }
+    }
+
+    public static void getEvents(WebDriver driver) {
+        try {
+            // Since DOM elements will be refreshed after Re-rendering and selenium requires sometime to save those changes!
+            Thread.sleep(5_000);
+        } catch(InterruptedException ie) {
+            System.out.println(ie.getMessage());
+        }
+        Events.selectThisWeekendButton(driver);
+        Events.applyPriceFilter(driver);
+        Events.saveEvents(driver);
+    }
+}
